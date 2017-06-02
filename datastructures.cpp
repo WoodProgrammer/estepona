afcf7bcb99b8517157685ca7341588ba513bc898
admanage <asddeneme3@gmail.com>
2017-06-03 01:55:16 +0300

    Struct Pointer ile örnekler yazıldı

--- datastructures.cpp
+++ datastructures.cpp
@@ -3,10 +3,13 @@ using namespace std;
 struct user{
   int user_id;
   string user_name;
-} user_container;
+};
+
+
 int main(){
-  user_container.user_name = "WoodProgrammer";
-  std::cout << user_container.user_name << '\n';
-
-
+  user user_container;
+  user *user_container_pointer;
+  user_container_pointer=&user_container;
+  int x  = user_container_pointer->user_id = 90;
+  std::cout << x << '\n';
 }
