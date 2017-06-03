5632d6cb6d6db3eab279a9f97d6116d846ea541d
admanage <asddeneme3@gmail.com>
2017-06-03 17:40:46 +0300

    Test için yeni şeyler eklendi

---
+++ socket/server.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<stdlib.h>
+#include<string.h>
+#define PORT 9090
+int main(){
+  int server_fd,new_socket,valread;
+  struct sockaddr_in_address;
+  
+}
