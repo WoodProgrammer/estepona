aacaf1c5d661def3a9884db3aa3d33fadbd0b5fd
admanage <asddeneme3@gmail.com>
2017-06-05 23:14:02 +0300

    Command Line Arguments Added

--- load_tester.c
+++ load_tester.c
@@ -12,10 +12,10 @@
 int main(int argc, char *argv[] ){
     int sockfd,counter = 0;
 
-
-
     struct sockaddr_in serverAddr;
     char *message = "HELLOSERVER";
+    char *server_ip_addr = argv[1];
+
 
     char str[1000];
     int coming_data, sending_data, structSize;
@@ -23,25 +23,25 @@ int main(int argc, char *argv[] ){
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(PORT);
-    serverAddr.sin_addr.s_addr = inet_addr(IP);
+    serverAddr.sin_addr.s_addr = inet_addr(server_ip_addr);
     memset(&(serverAddr.sin_zero), '\0', 8);
 
     if(connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(struct sockaddr)) == -1 ){
         perror("connect");
     }
 
-    int num = atoi(argv[1]);
+    int num = atoi(argv[2]);
     if(num!=13){
       while(counter<= num){
         sending_data  = send(sockfd, message, strlen(message), 0);
-        printf("SENT %lu bit data to %s:%d \n", strlen(message),IP,PORT );
+        printf("SENT %lu bit data to %s:%d \n", strlen(message),server_ip_addr,PORT );
         counter++;
       }
     }
     else{
       while(1){
         sending_data  = send(sockfd, message, strlen(message), 0);
-        printf("SENT %lu bit data to %s:%d \n", strlen(message),IP,PORT );
+        printf("SENT %lu bit data to %s:%d \n", strlen(message),server_ip_addr,PORT );
 
       }
     }
