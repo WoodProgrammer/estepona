7a11e26fcf4ab190eab6c0ac7523612c546e6986
admanage <asddeneme3@gmail.com>
2017-06-05 05:33:09 +0300

    Ping is changed

--- ping.c
+++ ping.c
@@ -5,28 +5,45 @@
 #include <netinet/in.h>
 #include <errno.h>
 
-#define SUNUCUPORT 5000
-#define SUNUCUIP "127.0.0.1"
-#define AZAMIUZUNLUK 8080
+#define PORT 5000
+#define IP "127.0.0.1"
+#define DESTINATION 8000
 
-int main(){
-    int sockfd;
+int main(int argc, char *argv[] ){
+    int sockfd,counter = 0;
+
+
+
     struct sockaddr_in serverAddr;
-    char *message = "HELLO SERVER ";
+    char *message = "HELLOSERVER";
+
     char str[1000];
     int coming_data, sending_data, structSize;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(SUNUCUPORT);
-    serverAddr.sin_addr.s_addr = inet_addr(SUNUCUIP);
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = inet_addr(IP);
     memset(&(serverAddr.sin_zero), '\0', 8);
 
-    if(-1 == connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(struct sockaddr))){
+    if(connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(struct sockaddr)) == -1 ){
         perror("connect");
     }
-    while(1){
-      sending_data  = send(sockfd, message, strlen(message), 0);
+
+    int num = atoi(argv[1]);
+    if(num!=13){
+      while(counter<= num){
+        sending_data  = send(sockfd, message, strlen(message), 0);
+        printf("SENT %lu bit data to %s:%d \n", strlen(message),IP,PORT );
+        counter++;
+      }
+    }
+    else{
+      while(1){
+        sending_data  = send(sockfd, message, strlen(message), 0);
+        printf("SENT %lu bit data to %s:%d \n", strlen(message),IP,PORT );
+
+      }
     }
   return 0;
 }
